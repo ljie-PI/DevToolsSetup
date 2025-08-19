fa6554f29b6c407c4596ad9e0d64eefbb92b7210
ljie-PI <ljie@lab440.tech>
2025-08-19 15:31:45 +0800

    Formatting

--- neovim/lua/plugins/lang/linting.lua
+++ neovim/lua/plugins/lang/linting.lua
@@ -1,24 +1,24 @@
 local M = {}
 
 M.options = {
-	events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-	linters_by_ft = {
-		bash = { "bash" },
-		javascript = { "eslint_d" },
-		lua = { "luacheck" },
-		python = { "pylint" },
-		typescript = { "eslint_d" },
-		typescriptreact = { "eslint_d" },
-	},
+  events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+  linters_by_ft = {
+    bash = { "bash" },
+    javascript = { "eslint_d" },
+    lua = { "luacheck" },
+    python = { "pylint" },
+    typescript = { "eslint_d" },
+    typescriptreact = { "eslint_d" },
+  },
 }
 
 M.setup = function(_, opts)
-	vim.api.nvim_create_autocmd(opts.events, {
-		group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-		callback = function()
-			require("lint").try_lint()
-		end,
-	})
+  vim.api.nvim_create_autocmd(opts.events, {
+    group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+    callback = function()
+      require("lint").try_lint()
+    end,
+  })
 end
 
 return M
